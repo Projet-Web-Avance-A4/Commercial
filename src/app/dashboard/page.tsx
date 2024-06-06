0c6356102b382fdc877b27c3e9a79812af2baac7
Vincent LELEU <vincent.leleu@viacesi.fr>
2024-06-06 11:39:56 +0200

    show order data from db not finished

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,10 +9,37 @@ import Footer from "../components/footer";
 import CustomCard from "../components/customcard";
 import PortalCard from "../components/portalCard";
 import Notification from "../components/notification";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaUserLarge , FaChartColumn } from 'react-icons/fa6';
+import { Order } from '../types/order';
 
 export default function Home() {
+
+
+  const [ordersList, setOrdersList] = useState<Order[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+      const fetchOrders = async () => {
+        try {
+          const response = await fetch('http://localhost:3001/api/order/getOrders', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+        const data = await response.json();
+        setOrdersList(data);
+        console.log(ordersList)
+        }
+
+        catch(err) {
+          console.error(err);
+        };
+      }
+        fetchOrders();
+    }, []);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -27,11 +54,24 @@ export default function Home() {
   return (
     <NextUIProvider className=" flex flex-col min-h-screen bg-beige">
       <Header title="Service Commercial" showMyAccount={true} showStats={false} showSponsor={true}/>
-      <div className="flex-grow my-5">
+      
+      <div className="text-black">
+      {ordersList? (
+        ordersList.map((order) => (
+          <div key={order.order_id}>
+            {/* Ajoutez d'autres champs si nécessaire */}
+          </div>
+        ))
+      ) : (
+        <div>No users found</div>
+      )}
+    </div>
+
+    {/*   <div className="flex-grow my-5">
         <Counter/>
         <OrderTable showAction={false} showStatusAction={true} showCreateAction={false} showEditAction={false} showDeleteAction={false}/>
       </div>
-      <Footer/>
+      <Footer/> */}
     </NextUIProvider>
   );
 }
