465561e8a2b679459c07b4810c5d97821bdaffa0
Vincent LELEU <vincent.leleu@viacesi.fr>
2024-06-13 14:18:41 +0200

    finish order dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { NextUIProvider } from "@nextui-org/system";
@@ -10,39 +9,38 @@ import { useState, useEffect } from 'react';
 import { Order } from '../types/order';
 import { Options } from "../interfaces/table";
 import { propsTable } from "@/app/interfaces/table";
-import deleteUserButton from "../components/actionButtonTable/deleteUserButton"
-import editUserButton from "../components/actionButtonTable/editUserButton"
-import switchUserState from "../components/actionButtonTable/switchStateButton"
+import deleteUserButton from "../components/actionButtonTable/deleteUserButton";
+import editUserButton from "../components/actionButtonTable/editUserButton";
+import switchUserState from "../components/actionButtonTable/switchStateButton";
 
 export default function Home() {
-
-
   const [ordersList, setOrdersList] = useState<Order[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-      const fetchOrders = async () => {
-        try {
-          const response = await fetch('http://localhost:3001/api/order/getOrders', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/api/order/getOrders', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          },
         });
         const data = await response.json();
-        console.log(data)
-        setOrdersList(data);
-        }
-
-        catch(err) {
-          console.error(err);
-        };
+        console.log(data);
+        const filteredOrders = data.filter((order: Order) => order.order_status !== "done");
+        setOrdersList(filteredOrders);
+      } catch (err) {
+        console.error(err);
+        setError("Failed to fetch orders.");
+      } finally {
+        setLoading(false);
       }
-        fetchOrders();
-    }, []);
+    };
+    fetchOrders();
+  }, []);
 
-    // Calculer la somme des prix des commandes sauf celles en statut "delivered"
   const totalOrderPrice = ordersList.reduce((total, order) => {
     if (order.order_status !== "delivered") {
       total += order.price;
@@ -61,63 +59,68 @@ export default function Home() {
   }
 
   const INITIAL_VISIBLE_COLUMNS = [
-    "adresse_resto",
-    "adresse_client",
-    "payment_method",
-    "actions",//Always here
+    "customer_name",
+    "order",
+    "restaurant_name",
+    "driver_name",
+    "city",
+    "price",
+    "order_status",
   ];
-  
-  const items = ordersList.map((order) => (
-    {
-      id: order.order_id,//We always need an unique id, but it is never shown. Make sure to used an unique key as value.
-      adresse_resto: order.restaurant.address.street + ', ' + order.restaurant.address.city,
-      ville_resto: order.restaurant.address.city,
-      adresse_client: order.customer.address.street + ', ' + order.customer.address.city,
-      ville_client: order.customer.address.city,
-      payment_method: order.payment.method=='credit_card'?'Carte de crédit':'Liquide',
-    }
-  ));
-  
+
+  const items = ordersList.map((order) => ({
+    id: order.order_id, 
+    customer_name: order.customer.name,
+    order: order.items.map(item => `${item.name}`).join(', '),
+    restaurant_name: order.restaurant.name,
+    driver_name: order.driver.name,
+    city: order.customer.address.city,
+    price: order.price,
+    order_status: order.order_status,
+  }));
+
   const columns = [
-    { name: "Adresse du restaurateur", uid: "adresse_resto" },
-    { name: "Ville du restaurateur", uid: "ville_resto" },
-    { name: "Adresse du client", uid: "adresse_client" },
-    { name: "Ville du client", uid: "ville_client" },
-    { name: "Moyen de paiement", uid: "payment_method" },
-    { name: "Actions", uid: "actions" },//Always here
+    { name: "Client", uid: "customer_name" },
+    { name: "Commande", uid: "order" },
+    { name: "Restaurant", uid: "restaurant_name" },
+    { name: "Livreur", uid: "driver_name" },
+    { name: "Ville", uid: "city" },
+    { name: "Prix", uid: "price" },
+    { name: "Status", uid: "order_status" }, 
   ];
-  
+
   const options: Options = {
-    content: "commande(s) trouvée(s)", //Used for the number of items and when no items was found
-    search_name: "Chercher par ville", //Title of the search bar
-    search_uid: ["ville_resto", "ville_client"], //ALWAYS AN ARRAY, uid of the column to filter 
-    selection_mode: "none", //"none", "single" or "multiple"
-    option_name: "Moyen de paiement", //Name of the option filter
-    option_uid: "payment_method", //ALWAYS A SINGLE STRING, uid of the column filtered with the option
+    content: "commande(s) trouvée(s)", 
+    search_name: "Chercher par client, restaurant, ville", 
+    search_uid: ["customer_name","restaurant_name", "city"], 
+    selection_mode: "none", 
+    option_name: "Status de la commande", 
+    option_uid: "order_status", 
     value_option: [
-      { name: "Carte de crédit", uid: "Carte de crédit" }, // uid need to be exactly the same as item's value. Name is the string to be printed
-      { name: "Liquide", uid: "Liquide" },
+      { name: "Reçue", uid: "checked" }, 
+      { name: "En préparation", uid: "in_progress" },
+      { name: "En livraison", uid: "in_delivery" },
     ],
   };
-  
+
   const props: propsTable = {
     columns: columns,
     options: options,
     items: items,
     INITIAL_VISIBLE_COLUMNS: INITIAL_VISIBLE_COLUMNS,
-  }
+  };
 
   return (
-    <NextUIProvider className=" flex flex-col min-h-screen bg-beige">
-      <Header title="Service Commercial" showMyAccount={false} showStats={false}/>
+    <NextUIProvider className="flex flex-col min-h-screen bg-beige">
+      <Header title="Service Commercial" showMyAccount={false} showStats={false} />
       <div className="flex-grow my-5">
-        <Counter totalOrderPrice={totalOrderPrice}/>
+        <Counter totalOrderPrice={totalOrderPrice} />
         <Table
-        props={props}
-        actionButtons={[switchUserState, editUserButton, deleteUserButton]}
-            />
+          props={props}
+          actionButtons={[]}
+        />
       </div>
-      <Footer/>
+      <Footer />
     </NextUIProvider>
   );
 }
